63874f601a5f7b6593f660e6eddc3954b97011be
Sebastian De La Roche <sebastian.delaroche@Sebastians-MacBook-Air.local>
2017-06-26 16:13:16 -0500

    feat: Changes on http.

--- App/Utils/Http.js
+++ App/Utils/Http.js
@@ -3,6 +3,9 @@ const HOST = `http://192.168.1.3`;
 
 const PORT = `3009`;
 
+const headers = {
+	'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+};
 
 function isEmptyObject(obj) {
   for(var i in obj) { return false; }
@@ -23,10 +26,7 @@ export default class Http {
 	static request(uri, method = 'GET', params = {}) {
 
 		let config = {
-			method,
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-			}
+			method, headers
 		};
 
 		if (!isEmptyObject(params))
